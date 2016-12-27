39b2e4ff8b21d9eed7d973266f39898b6de8b375
Alan Fidelino <afidedev26@gmail.com>
2016-12-27 23:24:01 +1100

    5.3 Set up templates

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
-
+const hbs     = require('hbs');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+hbs.registerPartials(__dirname + '/views/partials');
 
-app.use(express.static('/public'))
+hbs.registerHelper('getYear', ()=>{
+  return new Date().getFullYear();
+});
+
+app.set("view engine", "hbs");
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req,res)=>{
+  res.render("home.hbs",{
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to my website"
+  });
+});
+
+app.get("/about", (req,res)=>{
+  res.render("about.hbs",{
+    pageTitle: "About Page",
+    welcomeMessage: "Welcome to the about page"
+  });
+});
 
 app.listen(port, ()=>{
   console.log(`Listening at port ${port}`);
